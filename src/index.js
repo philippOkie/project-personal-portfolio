bf4c96379139f5ce8ad40a0294a5a51ac2f23cad
philippOkie <kriukphilipp@gmail.com>
2023-10-20 00:13:23 +0300

    add copy email on click, some more animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles/main.scss";
+
+const getEmailBtn = document.getElementById("getEmailBtn");
+const myEmail = "kriukphilipp@gmail.com";
+const textArea = document.createElement("textarea");
+
+getEmailBtn.addEventListener("click", () => {
+  textArea.value = myEmail;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+});
